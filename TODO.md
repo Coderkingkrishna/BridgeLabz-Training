b558265406e308d42e8b7e27c2f16b478dcb1223
Coderkingkrishna <agrawalkrishna038@gmail.com>
2025-12-24 15:02:12 +0530

    [Krishna Agrawal] Update

--- TODO.md
+++
@@ -1,3 +0,0 @@
-# TODO for Updating README for Today's Commit
-
-- [x] Append Day 04 section to README.md, including overview, programs implemented, theoretical concepts, key takeaways, and conclusion for array programs.
--- core-csharp-practice/gcr-codebase/csharp-arrays/level2/Salary.cs
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level2/Salary.cs
@@ -1,21 +1,21 @@
 using System;
 class Salary{
     public static void Main(string[] args){
-        var Data = new double[10, 5];
-        for (var i = 0; i < 10; i++){
-            var SalaryAmount = double.Parse(Console.ReadLine());
-            var Year = double.Parse(Console.ReadLine());
+        double[ ,] Data = new double[10, 5];
+        for (int i = 0; i < 10; i++){
+            double SalaryAmount = double.Parse(Console.ReadLine());
+            double Year = double.Parse(Console.ReadLine());
             Data[i, 0] = SalaryAmount;
             Data[i, 1] = Year;
-            var Bonus = Year < 5? (SalaryAmount * 2) / 100 : (SalaryAmount * 5) / 100;
+            double Bonus = Year < 5? (SalaryAmount * 2) / 100 : (SalaryAmount * 5) / 100;
             Data[i, 2] = Bonus;
-            var OldSalary = SalaryAmount * Year;
-            var NewSalary = OldSalary + Bonus;
+            double OldSalary = SalaryAmount * Year;
+            double NewSalary = OldSalary + Bonus;
             Data[i, 3] = OldSalary;
             Data[i, 4] = NewSalary;
         }
 
-        for (var i = 0; i < Data.GetLength(0); i++){
+        for (int i = 0; i < Data.GetLength(0); i++){
             Console.WriteLine("Bonus = " + Data[i, 2] + " OldSalary = " + Data[i, 3] +"  NewSalary = " + Data[i, 4]);
         }
     }
